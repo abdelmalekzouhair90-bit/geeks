aa6271ff18a9cc966ff049d9a76823d151c8dda5
Abdelmalek zouhair <abdelmalekzouhair90@gmail.com>
2026-02-13 01:45:08 +0100

    add EX: 4

--- week_2/day_2/exercises/exercises_xp.js
+++ week_2/day_2/exercises/exercises_xp.js
@@ -56,3 +56,30 @@ console.log([...country]);
 //  Bonus 
 let newArray = [...[,,]];
 console.log(newArray);
+
+
+
+
+// ===== Exercise 4
+
+const users = [
+  { firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+  { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+  { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+  { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+  { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+  { firstName: 'Wes', lastName: 'Reid', role: 'Instructor' },
+  { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor' }
+];
+
+const welcomeStudents = users.map(user => `Hello ${user.firstName}`);
+
+console.log(welcomeStudents);
+
+const fullStackResidents = users.filter(user => user.role === 'Full Stack Resident');
+console.log(fullStackResidents);
+
+const residentLastNames = users
+  .filter(user => user.role === 'Full Stack Resident')
+  .map(user => user.lastName);
+console.log(residentLastNames);
