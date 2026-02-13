c212961c1ff16ad15400b1394acc31d0a844fd55
Abdelmalek zouhair <abdelmalekzouhair90@gmail.com>
2026-02-13 06:09:36 +0100

    add execises.  EX: 4, EX: 5, EX: 6

--- week_2/day_3/exercises/exercises_xp.js
+++ week_2/day_3/exercises/exercises_xp.js
@@ -25,3 +25,65 @@ const doubledUsers = usersArray.map(([key, value]) => [key, value * 2]);
 console.log(doubledUsers);
 
 
+// ===== Exercise 4
+
+/* 
+The variable member is an instance of the Person class.
+ In JavaScript, all class instances are objects, 
+ so when we use typeof on it, it returns "object" because typeof only distinguishes primitive types from objects, 
+not the specific class.
+
+so the Output:  object
+*/
+
+// ===== Exercise 5
+
+class Labrador extends Dog {
+  constructor(name, size) {
+    this.name = name;
+    this.size = size;
+  }
+  
+  // the Correct Answer because : Calls super(name) first → initializes the parent (Dog) class. Then this.size = size is safe
+
+
+};
+
+
+// ===== Exercise 6
+
+
+console.log([2] === [2]);
+console.log({} === {});
+
+const object1 = { number: 5 }; 
+const object2 = object1; 
+const object3 = object2; 
+const object4 = { number: 5 };
+
+object1.number = 4;
+
+console.log(object2.number);
+console.log(object3.number);
+console.log(object4.number);
+
+class Animal {
+  constructor(name, type, color) {
+    this.name = name;
+    this.type = type;
+    this.color = color;
+  }
+}
+
+class Mammal extends Animal {
+  sound(soundStr) {
+    return `${soundStr} I'm a ${this.type}, named ${this.name} and I'm ${this.color}`;
+  }
+}
+
+const farmerCow = new Mammal('Lily', 'cow', 'brown and white');
+console.log(farmerCow.sound('Moooo'));
+
+
+// ===== Exercise 7
+
