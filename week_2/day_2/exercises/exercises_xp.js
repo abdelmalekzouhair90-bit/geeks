b13e8621f05c56baf4a7fcf51f654e5c8fcf5ca2
Abdelmalek zouhair <abdelmalekzouhair90@gmail.com>
2026-02-13 01:24:59 +0100

    add EX: 2

--- week_2/day_2/exercises/exercises_xp.js
+++ week_2/day_2/exercises/exercises_xp.js
@@ -18,3 +18,22 @@ const hasViolet = colors.some(color => color === "Violet");
 
 console.log(hasViolet ? "Yeah" : "No...");
 
+// ===== Exercise 2
+
+
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+const ordinal = ["th","st","nd","rd"];
+
+
+
+colors.forEach((color, index) => {
+  const position = index + 1;
+
+  const suffix =
+    position === 1 ? ordinal[1] :
+    position === 2 ? ordinal[2] :
+    position === 3 ? ordinal[3] :
+    ordinal[0];
+
+  console.log(${position}${suffix} choice is ${color}.);
+});
